3f0ce37b9e2b09aedbbbc1d832ff2e7a77d3b8f6
Ramon William <roniewill@gmail.com>
2020-05-03 17:54:18 -0300

    feat: add studies about relational assignment

---
+++ fundamentals/relational/relational.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		relational assignment
+		==  equal to
+		!= different than
+		>  bigger then
+		<  less than
+		>= greater than or equal to
+		<= less than or equal to
+	*/
+	num1 := 5
+	num2 := 10
+	fmt.Println("=", num1 == num2)
+	fmt.Println("!=", num1 != num2)
+	fmt.Println(">", num1 > num2)
+	fmt.Println("<", num1 < num2)
+	fmt.Println(">=", num1 >= num2)
+	fmt.Println("<=", num1 <= num2)
+}
