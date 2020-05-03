9f8d335b48ff18f3bcde8b6a785d62cdafacfe96
Ramon William <roniewill@gmail.com>
2020-05-03 18:55:36 -0300

    feat: add studies about: Golang have no ternary operator

--- fundamentals/operators/ternary/ternary.go
+++ fundamentals/operators/ternary/ternary.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+/*
+	WARNING!!! The Go programming language has no ternary operator.
+	But, we will show an alternative
+*/
+
+func getResult(score float64) string {
+	if score >= 6 {
+		return "Approved"
+	}
+	return "disapproved"
+}
+
 func main() {
-	fmt.Println("")
+	score := 6.5
+	fmt.Println(getResult(score))
 }
