d1f34661d83cf7570fe070edeee97122ebcdf0ff
Ramon William <roniewill@gmail.com>
2020-05-03 18:44:14 -0300

    feat: add studies about unary operator

--- fundamentals/operators/unary/unary.go
+++ fundamentals/operators/unary/unary.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("")
+	/*
+		unary operators
+	*/
+	x := 1
+	y := 2
+
+	// only post fixed
+	x++ // x += 1 or x = x + 1
+	fmt.Println(x)
+
+	y-- // y -= 1 or y = y - 1
+	fmt.Println(y)
+
+	/*
+		WARNING...
+		Don't do that
+		fmt.Println(x == y--)
+	*/
 }
